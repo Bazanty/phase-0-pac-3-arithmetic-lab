f7497b796a76543dc75392034aa0d1353734fe42
bazanty <bazantyloffan@gmail.com>
2024-09-19 16:20:58 +0300

    ARITHMETIC

---
+++ index.js
@@ -0,0 +1,53 @@
+// function to compare the product of x and y
+function add(x, y){
+    return x+y;
+}
+function subtract(x, y){
+    return x-y;
+}
+
+function multiply(x, y){
+    return x*y;
+}
+function divide(x, y){
+    return x/y;
+
+}
+    // we'll fill this in shortly
+    
+
+function increment(n) {
+  return n + 1;
+}
+
+
+function decrement(n) {
+    return n-1
+}
+function makeInt(string){
+
+}
+function add5() {
+    return (number += 5);
+}
+
+function divideBy3() {
+    return (number /= 3);
+}
+
+function makeInt(n){
+    return (n);
+}
+
+function makeInt(n){
+    return parseInt(n, 10);
+    }
+function preserveDecimal(n){
+    return parseInt(n)
+}
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+
+
